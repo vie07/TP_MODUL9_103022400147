2a916c5e4b5d273370a983551c2f2abb11127a4d
Ravie <aryaravie8@gmail.com>
2026-04-27 05:00:12 +0700

    Menambahkan Implementasi Runtime Configuration

---
+++ TP_MODUL9_103022400147/TP_MODUL9_103022400147/CovidConfig.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+
+namespace TP_MODUL9_103022400147
+{
+    internal class CovidConfig
+    {
+        public string satuan_suhu { get; set; }
+        public int batas_hari_demam { get; set; }
+        public string pesan_ditolak { get; set; }
+        public string pesan_diterima { get; set; }
+
+        public static CovidConfig Load()
+        {
+            string path = "covid_config.json";
+
+            string json = File.ReadAllText(path);
+            return JsonSerializer.Deserialize<CovidConfig>(json);
+        }
+
+        public void UbahSatuan()
+        {
+            if (satuan_suhu == "celcius")
+                satuan_suhu = "fahrenheit";
+            else
+                satuan_suhu = "celcius";
+        }
+    }
+}
--- TP_MODUL9_103022400147/TP_MODUL9_103022400147/Program.cs
+++ TP_MODUL9_103022400147/TP_MODUL9_103022400147/Program.cs
@@ -1,1 +1,40 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+using TP_MODUL9_103022400147;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        CovidConfig config = CovidConfig.Load();
+
+        Console.WriteLine($"Berapa suhu badan anda saat ini? Dalam nilai {config.satuan_suhu}");
+        double suhu = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Berapa hari yang lalu (perkiraan) anda terakhir memiliki gejala demam?");
+        int hari = Convert.ToInt32(Console.ReadLine());
+
+        bool suhuValid = false;
+
+        if (config.satuan_suhu == "celcius")
+        {
+            suhuValid = (suhu >= 36.5 && suhu <= 37.5);
+        }
+        else
+        {
+            suhuValid = (suhu >= 97.7 && suhu <= 99.5);
+        }
+
+        bool hariValid = hari < config.batas_hari_demam;
+
+        if (suhuValid && hariValid)
+        {
+            Console.WriteLine(config.pesan_diterima);
+        }
+        else
+        {
+            Console.WriteLine(config.pesan_ditolak);
+        }
+
+        Console.WriteLine($"Satuan suhu sekarang: {config.satuan_suhu}");
+    }
+}
